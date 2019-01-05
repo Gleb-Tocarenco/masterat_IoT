8b2cc084826a26b0fcf50e028a34cbadfbc1406e
Gleb-Tocarenco <gleb.tocarenco@gmail.com>
2019-01-06 01:50:04 +0200

    changed amplitude to float

--- earthquake_collector/models.py
+++ earthquake_collector/models.py
@@ -8,7 +8,7 @@ class SeismicRecord(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     device_id = models.CharField(max_length=255)
-    amplitude = models.IntegerField(default=5)
+    amplitude = models.FloatField(default=5)
     added_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- earthquake_collector/signals.py
+++ earthquake_collector/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 
+import telegram
 from .models import SeismicRecord, SeismicNotification
 
 
@@ -7,7 +8,9 @@ def send_notification(sender, instance, **kwargs):
     notification, _ = SeismicNotification.objects.get_or_create(amplitude=5)
 
     if instance.amplitude >= notification.amplitude:
-        print('send notification')
+        bot = telegram.Bot(token='665012119:AAHpeitpB1cRpSRzFs2r_6gVVZq7B58k6EM')
+        bot.send_message(chat_id='@seismic_record', text=f'New seismic record has been recorded with '
+                                                         f'amplitude of {instance.amplitude}')
 
 
 post_save.connect(send_notification, SeismicRecord)
